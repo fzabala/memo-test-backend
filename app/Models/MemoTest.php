cd380de0bc95b6d0809e7bd46249ba56c96a78cd
Franco Zabala <asfalso@gmail.com>
2024-04-29 00:31:51 -0300

    add active game session to memo test model

--- app/Models/MemoTest.php
+++ app/Models/MemoTest.php
@@ -9,9 +9,20 @@ class MemoTest extends Model
 {
 
     protected $fillable = ["name", "highest_score"];
+    protected $appends = ['active_game_session'];
 
     public function images()
     {
         return $this->hasMany(Image::class);
+    }
+
+    public function gameSessions()
+    {
+        return $this->hasMany(GameSession::class);
+    }
+
+    public function getActiveGameSessionAttribute()
+    {
+        return $this->gameSessions()->where('state', 'STARTED')->first();
     }
 }
