2880ff2239f7e153c9b94987a5f6e493de309131
Franco Zabala <asfalso@gmail.com>
2024-04-29 02:04:19 -0300

    add transactions to delete operations

--- app/GraphQL/Mutations/DeleteMemoTestById.php
+++ app/GraphQL/Mutations/DeleteMemoTestById.php
@@ -9,17 +9,20 @@ use App\Models\GameSession;
 use App\Models\MemoTest;
 use App\Models\Image;
 use Exception;
+use Illuminate\Support\Facades\DB;
 
 final readonly class DeleteMemoTestById
 {
     /** @param  array{}  $args */
     public function __invoke(null $_, array $args)
     {
-        $imageIds = Image::where('memo_test_id', $args['id'])->select('id');
-        CardGameSession::whereIn('image_id', $imageIds)->delete();
-        Image::where('memo_test_id', $args['id'])->delete();
-        GameSession::where('memo_test_id', $args['id'])->delete();
-        MemoTest::where('id', $args['id'])->delete();
-        return true;
+        return DB::transaction(function () use ($args) {
+            $imageIds = Image::where('memo_test_id', $args['id'])->select('id');
+            CardGameSession::whereIn('image_id', $imageIds)->delete();
+            Image::where('memo_test_id', $args['id'])->delete();
+            GameSession::where('memo_test_id', $args['id'])->delete();
+            MemoTest::where('id', $args['id'])->delete();
+            return true;
+        });
     }
 }
--- app/GraphQL/Mutations/RemoveImageFromMemoTestById.php
+++ app/GraphQL/Mutations/RemoveImageFromMemoTestById.php
@@ -4,27 +4,31 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Mutations;
 
-use App\Models\Image;
 use App\Models\MemoTest;
+use App\Models\CardGameSession;
 use Exception;
+use Illuminate\Support\Facades\DB;
 
 final readonly class RemoveImageFromMemoTestById
 {
     /** @param  array{}  $args */
     public function __invoke(null $_, array $args)
     {
-        $memoTest = MemoTest::find($args['id']);
-        if (!$memoTest) {
-            throw new Exception("MemoTest not found");
-        }
+        return DB::transaction(function () use ($args) {
+            $memoTest = MemoTest::find($args['id']);
+            if (!$memoTest) {
+                throw new Exception("MemoTest not found");
+            }
 
-        $imageToDestroy = $memoTest->images()->where('id', $args['imageId'])->first();
-        if (!$imageToDestroy) {
-            throw new Exception("Image not found");
-        }
+            $imageToDestroy = $memoTest->images()->where('id', $args['imageId'])->first();
+            if (!$imageToDestroy) {
+                throw new Exception("Image not found");
+            }
 
-        $imageToDestroy->delete();
+            CardGameSession::where('image_id', $imageToDestroy->id)->delete();
+            $imageToDestroy->delete();
 
-        return $memoTest->load('images');
+            return $memoTest->load('images');
+        });
     }
 }
